63d098050396faf0cd7d31a472928c304663e849
bruce <bruce>
2022-12-11 22:28:11 +0800

    add instance

---
+++ src/instance/MyInstance.java
@@ -0,0 +1,9 @@
+package instance;
+
+public class MyInstance {
+    public static void main(String[] args) {
+        MyInstance m1 = new MyInstance();
+        MyInstance m2 = new MyInstance();
+        MyInstance m3 = new MyInstance();
+    }
+}
